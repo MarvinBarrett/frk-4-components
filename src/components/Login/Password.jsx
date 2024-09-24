040fa97d7c554de0af26497f9253901a48538f32
Marvin Barrett <marvin.barrett@mb@gmail.com>
2024-09-24 14:24:10 +0200

    blabla

---
+++ src/components/Login/Password.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Password = () =>{
+    return(
+        <div>
+            Password
+            <input type='password' />
+        </div>
+    )
+    
+}
+
+export default Password;
